ef106334127ba5c3242bc34192831c93bd78ae2b
carleandro7 <carleandro7@gmail.com>
2025-09-16 08:24:06 -0300

    Refactor login redirect and remove unused CSS files
    - Updated the login view to redirect users to 'area_aluno' instead of 'lista_alunos' upon successful authentication.
    - Deleted obsolete scaffolds.scss and summernote-lite.min.scss files to clean up the codebase and reduce unnecessary dependencies.

--- escola/views.py
+++ escola/views.py
@@ -355,7 +355,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('lista_alunos')
+            return redirect('area_aluno')
         else:
             return render(request, 'login.html', {'erro': 'Usuário ou senha inválidos'})
     return render(request, 'login.html')
